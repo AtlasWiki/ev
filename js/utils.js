1f490cfb23c6bea5cdb7c2080097181edc2134a9
AtlasWiki <nathanwong3421@gmail.com>
2024-11-25 21:30:01 -0800

    add clear button for current XP and Goals

--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,15 @@
 const clubLvlsArray = Object.entries(clubLvls);
 const sectionItems = Object.entries(sections)
 const descriptionItems = Object.entries(descriptions)
+
+function clearValue(id) {
+    const ids = Array.isArray(id) ? id : [id]; // Convert to array if it's a single value
+    
+    ids.forEach((id) => {
+        let element = document.getElementById(id);
+        if (element) {
+            element.innerHTML = "";
+            element.value = "";
+        }
+    });
+}
