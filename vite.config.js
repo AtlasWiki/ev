ec3c0fab9ea5920e53fbea6ef7f6070177560097
AtlasWiki <nathanwong3421@gmail.com>
2024-11-26 17:57:10 -0800

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './', // Ensure Vite looks for your files in the root folder of the project
-  base: './', // Ensures that Vite looks for assets relative to the root
+  root: './',
+  base: './', 
   build: {
-    outDir: 'dist', // Folder where the bundled files will be output
+    outDir: 'dist', 
     rollupOptions: {
-      input: './index.html', // Correct path to the index.html as the entry point
+      input: './index.html',
     },
-    minify: true, // Disable minification to simplify debugging
-     // Enable source maps for easier debugging
+    minify: true, 
   },
 });
