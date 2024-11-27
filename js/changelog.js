312fcbe84f116231b1a4893ae40c7e323c485f9c
AtlasWiki <nathanwong3421@gmail.com>
2024-11-27 02:07:42 -0800

    update changelog

--- js/changelog.js
+++ js/changelog.js
@@ -1,7 +1,8 @@
 export const changelog = {
     "v0.1.0": { title:"Initial Release", date: "11/24/2024", url: "https://github.com/AtlasWiki/ev/releases/tag/v0.1.0" },
     "v0.2.0": { title:"Club Rewards", date: "11/25/2024", url:"https://github.com/AtlasWiki/ev/releases/tag/v0.2.0" },
-    'v0.3.0': { title:"Accessibility and Bug fixes", date: "11/25/2024", url:"https://github.com/AtlasWiki/ev/releases/tag/v0.3.0" }
+    'v0.3.0': { title:"Accessibility and Bug fixes", date: "11/25/2024", url:"https://github.com/AtlasWiki/ev/releases/tag/v0.3.0" },
+    'v1.0.0': { title:"Performance and Offline Viewing", date: "11/26/2024", url:"https://github.com/AtlasWiki/ev/releases/tag/v1.0.0" },
 }
 
 export function createLog(){
